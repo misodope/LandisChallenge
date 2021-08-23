ead1c74db52ec0bfc8c24a4e2e8758497318623c
Jerry Chen <misodope@cap-rx.com>
2021-08-23 01:36:45 -0400

    bap

--- app/frontend/src/api/service.js
+++ app/frontend/src/api/service.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const baseUrl = process.env.BASE_URL || 'http://localhost:1337';
+const baseUrl = process.env.NODE_ENV === 'production' ?  window.location.protocol + "//" + window.location.host : 'http://localhost:1337';
 
 const post = (url, params = {}) => {
-  console.log(process.env);
   console.log(baseUrl + url);
   return axios({
     method: 'post',
